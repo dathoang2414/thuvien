3ab6bdda09f322e49c4e12fb49382a077801db22
Hoàng Quốc Đạt <89507944+dathoang2414@users.noreply.github.com>
2023-09-21 16:43:44 +0700

    ok

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'libsystem');
+	$conn = new mysqli('localhost', 'root', '12345678', 'libsystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
